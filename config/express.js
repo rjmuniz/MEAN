468709444f100a9779a5c62343e05adb447219a3
rjmuniz <rjmuniz@gmail.com>
2016-03-31 20:47:07 -0300

    Fim capitulo 2

--- config/express.js
+++ config/express.js
@@ -1,17 +1,24 @@
+var load = require('express-load');
+var bodyParser = require('body-parser');
 var express = require('express');
-var home = require('../app/routes/home');
 
-module.exports = function(){
+
+module.exports = function() {
   var app = express();
-  
+
   app.set('port', 3000);
-  
+
   app.use(express.static('./public'));
   app.set('view engine', 'ejs')
   app.set('views', './app/views');
-  
-  
-  home(app);
-  
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
+
+  load('models', { cwd: 'app' }) //load models in folder app
+		.then('controllers')
+		.then('routes')
+		.into(app);
+
   return app;
 };
