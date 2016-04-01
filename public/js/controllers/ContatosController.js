6ae9c6ed6088f7d7193dce97760d1ed5cb72fc30
rjmuniz <rjmuniz@gmail.com>
2016-04-01 18:47:14 -0300

    atualizar 4.10

---
+++ public/js/controllers/ContatosController.js
@@ -0,0 +1,6 @@
+console.log('Controller - starting');
+angular.module('contatooh').controller('ContatosController',
+	function($scope) {
+		$scope.total = 0;
+	});
+	console.log('Controller - started');
---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+console.log('Main - starting');
+angular.module('contatooh', []);
+console.log('Main - started');
