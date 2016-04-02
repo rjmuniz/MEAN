5e5dc43e196ace423a781df5bdc6b10b78758945
rjmuniz <rjmuniz@gmail.com>
2016-04-02 12:03:03 -0300

    atualizado até o Cap 4.20

---
+++ public/js/controllers/ContatoController.js
@@ -0,0 +1,5 @@
+angular.module('contatooh').controller('ContatoController',
+	function ControllerController($scope, $routeParams) {
+		console.log($routeParams.contatoId);
+		
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
-console.log('Main - starting');
-angular.module('contatooh', []);
-console.log('Main - started');
+angular.module('contatooh', ['ngRoute'])
+	.config(function($routeProvider) {
+
+		$routeProvider
+			.when('/contatos', {
+				templateUrl: 'partials/contatos.html',
+				controller: 'ContatosController'
+			})
+			.when('/contato/:contatoId', {
+				templateUrl: 'partials/contato.html',
+				controller: 'ContatoController'
+			})
+			.otherwise({ redirectTo: '/contatos' });
+
+	});
