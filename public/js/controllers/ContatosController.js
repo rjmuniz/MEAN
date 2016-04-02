346cf78b6a244a8038e3d20003b1e620dfa131f5
rjmuniz <rjmuniz@gmail.com>
2016-04-02 11:10:46 -0300

    Cap 4.4

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -2,5 +2,8 @@ console.log('Controller - starting');
 angular.module('contatooh').controller('ContatosController',
 	function($scope) {
 		$scope.total = 0;
+		$scope.incrementa = function(){
+			$scope.total++;
+		};
 	});
 	console.log('Controller - started');
