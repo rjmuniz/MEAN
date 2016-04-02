ad4689f487bcf78e4ec21799b8568835976f044a
rjmuniz <rjmuniz@gmail.com>
2016-04-02 12:45:43 -0300

    finalizado 5.5 - pg 97

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,25 +1,16 @@
 angular.module('contatooh').controller('ContatosController',
-	function($scope) {
-		$scope.total = 0;
-		$scope.incrementa = function() {
-			$scope.total++;
-		};
+	function($scope, $http) {
+		$scope.contatos = [];
+		
 		$scope.filtro = '';
-		$scope.contatos = [
-			{
-				"_id": 1,
-				"nome": "Contato Angular 1",
-				"email": "cont1@empresa.com.br"
-			},
-			{
-				"_id": 2,
-				"nome": "Contato Angular 2",
-				"email": "cont2@empresa.com.br"
-			},
-			{
-				"_id": 3,
-				"nome": "Contato Angular 3",
-				"email": "cont3@empresa.com.br"
-			}
-		];
+
+		$http.get('/contatos')
+			.success(function(data) {
+				$scope.contatos = data;
+			 })
+			.error(function(statusText) {
+				console.log('Não foi possível obter a lista de contatos');
+				console.log(statusText);
+			});
+
 	});
