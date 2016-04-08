6cfeb57e26a0d5cb544dce596c7b4e72d79c13e6
rjmuniz <rjmuniz@gmail.com>
2016-04-08 11:35:07 -0300

    atualizado - cap7,3

--- consulta.js
+++ consulta.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 
 var _idProcurado = new ObjectID('5707b6635ac65cde9ec2d596');
-
+//rodar script.nsql
 MongoClient.connect('mongodb://127.0.0.1:27017/contatooh', function(erro, db) {
 	if (erro) throw err;
 	db.collection('contatos').findOne({ _id: _idProcurado }, function(erro, contato) {
